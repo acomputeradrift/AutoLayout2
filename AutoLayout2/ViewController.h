459545939790b9de734e9145178100121f971bc6
Jamie Feeny <feeny.jamie@gmail.com>
2018-08-12 09:36:13 -0700

    Initial Commit

---
+++ AutoLayout2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout2
+//
+//  Created by Jamie on 2018-08-12.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
